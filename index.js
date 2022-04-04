0850b8adf57c891c71d4305b88f7807bb77e4de0
Kane Mathis <admin@kanemathis.com>
2022-04-04 13:28:55 -0700

    commit tree 89a63f00153fba22120612a303acf22928b33c54

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+let totalBatteries =[]
+batteries= batteryBatches.reduce(function(total, next){
+   let result = total + next; totalBatteries=result; return totalBatteries;
+}, 0);
+
+
+
+console.log(batteries);
+
